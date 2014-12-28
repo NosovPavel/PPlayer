fd7357901a597667b77e61b03a9240f7b4335a8b
Alexander Orlov <me@orlovalex.com>
2014-12-28 14:33:26 +0300

    Initial Commit

---
+++ PPlayer/Other/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PPlayer
+//
+//  Created by Alexander Orlov on 28.12.14.
+//  Copyright (c) 2014 Alexander Orlov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
