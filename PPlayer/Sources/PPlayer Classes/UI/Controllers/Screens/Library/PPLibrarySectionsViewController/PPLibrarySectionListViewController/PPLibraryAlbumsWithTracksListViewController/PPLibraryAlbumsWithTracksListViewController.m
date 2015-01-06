64c0b3b7e10d4a49d558edd69d6c488d361ffead
Alexander Orlov <me@orlovalex.com>
2015-01-07 01:29:21 +0300

    Perfect albums with tracks list controller.

--- PPlayer/Sources/PPlayer Classes/UI/Controllers/Screens/Library/PPLibrarySectionsViewController/PPLibrarySectionListViewController/PPLibraryAlbumsWithTracksListViewController/PPLibraryAlbumsWithTracksListViewController.m
+++ PPlayer/Sources/PPlayer Classes/UI/Controllers/Screens/Library/PPLibrarySectionsViewController/PPLibrarySectionListViewController/PPLibraryAlbumsWithTracksListViewController/PPLibraryAlbumsWithTracksListViewController.m
@@ -21,14 +21,16 @@
 
 #import "PPLibraryAlbumsWithTracksListViewController.h"
 #import "PPLibraryProvider.h"
-#import "PPLibraryAlbumModel.h"
-#import "PPLibraryGenreModel.h"
 
 static const CGFloat cellsHeight = 50.0f;
 static NSString *tracksCellIdentifier = @"tracksCellIdentifier";
 
-static const CGFloat headersHeight = 100.0f;
+static const CGFloat headersHeight = 80.0f;
 static NSString *albumsHeaderIdentifier = @"albumsHeaderIdentifier";
+
+UIEdgeInsets edgeInsets() {
+    return UIEdgeInsetsMake(0.0f, 10.0f, 0.0f, 10.0f);
+}
 
 @implementation PPLibraryAlbumsWithTracksCell
 
@@ -38,6 +40,11 @@ static NSString *albumsHeaderIdentifier = @"albumsHeaderIdentifier";
     [self.imageView setContentMode:UIViewContentModeCenter];
     [self.textLabel setFont:[UIFont boldSystemFontOfSize:17.0f]];
     [self.detailTextLabel setFont:[UIFont systemFontOfSize:13.0f]];
+
+    if ([self respondsToSelector:@selector(setPreservesSuperviewLayoutMargins:)]) {
+        [self setPreservesSuperviewLayoutMargins:NO];
+    }
+    self.separatorInset = edgeInsets();
 }
 
 #pragma mark - Lifecycle
@@ -51,9 +58,109 @@ static NSString *albumsHeaderIdentifier = @"albumsHeaderIdentifier";
     return self;
 }
 
+#pragma mark - Layout
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+}
+
+
+@end
+
+@interface PPLibraryAlbumsWithTracksHeaderView () {
+@private
+    UIView *_bottomLine, *_topLine;
+    UIImageView *_artworkView;
+    UILabel *_titleLabel, *_subtitleLabel;
+}
+@property(atomic) float separatorInsetLeft;
+@property(atomic, strong) UILabel *titleLabel, *subtitleLabel;
 @end
 
 @implementation PPLibraryAlbumsWithTracksHeaderView
+@synthesize titleLabel = _titleLabel;
+@synthesize subtitleLabel = _subtitleLabel;
+
+#pragma mark - Init
+
+- (void)_init {
+    self.contentView.backgroundColor = [UIColor whiteColor];
+
+    _bottomLine = [[UIView alloc] init];
+    [_bottomLine setBackgroundColor:[UIColor colorWithWhite:0.89f alpha:1.0f]];
+
+    [self.contentView addSubview:_bottomLine];
+
+    _topLine = [[UIView alloc] init];
+    [_topLine setBackgroundColor:[UIColor colorWithWhite:0.89f alpha:1.0f]];
+
+    [self.contentView addSubview:_topLine];
+
+    _artworkView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ArtworkPlaceHolderBig.png"]];
+    [_artworkView setContentMode:UIViewContentModeScaleToFill];
+
+    [self.contentView addSubview:_artworkView];
+
+    _titleLabel = [[UILabel alloc] init];
+    [self.contentView addSubview:_titleLabel];
+
+    _subtitleLabel = [[UILabel alloc] init];
+    [self.contentView addSubview:_subtitleLabel];
+
+    [self.titleLabel setFont:[UIFont boldSystemFontOfSize:17.0f]];
+    [self.subtitleLabel setFont:[UIFont systemFontOfSize:13.0f]];
+}
+
+#pragma mark - Lifecycle
+
+- (instancetype)initWithReuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithReuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self _init];
+    }
+
+    return self;
+}
+
+- (void)dealloc {
+    _bottomLine = nil;
+    _topLine = nil;
+    _artworkView = nil;
+    _titleLabel = nil;
+    _subtitleLabel = nil;
+}
+
+#pragma mark - Layout
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    [_bottomLine setFrame:CGRectMake(0.0f, self.contentView.bounds.size.height - 0.5f,
+            self.contentView.bounds.size.width,
+            0.5f)];
+    [_topLine setFrame:CGRectMake(0.0f, 0.1f,
+            self.contentView.bounds.size.width,
+            0.5f)];
+
+    CGFloat padding = _separatorInsetLeft;
+    CGFloat artworkSize = (self.contentView.bounds.size.height - 1.0f) - padding * 2.0f;
+
+    [_artworkView setFrame:CGRectMake(padding, padding,
+            artworkSize, artworkSize)];
+
+    [_titleLabel sizeToFit];
+    [_subtitleLabel sizeToFit];
+
+    [_titleLabel setFrame:CGRectMake(padding + artworkSize + padding,
+            _artworkView.center.y - _titleLabel.bounds.size.height + 2.0f,
+            self.contentView.bounds.size.width - padding - artworkSize - padding - padding,
+            _titleLabel.bounds.size.height)];
+
+    [_subtitleLabel setFrame:CGRectMake(padding + artworkSize + padding,
+            _artworkView.center.y + 2.0f,
+            self.contentView.bounds.size.width - padding - artworkSize - padding - padding,
+            _subtitleLabel.bounds.size.height)];
+}
+
 @end
 
 @interface PPLibraryAlbumsWithTracksListViewController () {
@@ -108,6 +215,7 @@ static NSString *albumsHeaderIdentifier = @"albumsHeaderIdentifier";
 - (void)commonInit {
     [super commonInit];
     _sourceTableView.rowHeight = cellsHeight;
+    _sourceTableView.separatorInset = edgeInsets();
 }
 
 #pragma mark - Reloading
@@ -185,11 +293,21 @@ static NSString *albumsHeaderIdentifier = @"albumsHeaderIdentifier";
     NSMutableArray *tracks = _tracksArray[(NSUInteger) indexPath.section];
     PPLibraryTrackModel *track = tracks[(NSUInteger) indexPath.row];
 
-    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%d  ", indexPath.row + 1]
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%lld  ", ((int64_t) (indexPath.row + 1))]
                                                                               attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:17.0f]}];
     [title appendAttributedString:[[NSAttributedString alloc] initWithString:track.title]];
 
     [cell.textLabel setAttributedText:title];
 };
+
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(PPLibraryAlbumsWithTracksHeaderView *)view forSection:(NSInteger)section {
+    PPLibraryAlbumModel *currentAlbum = _sourceArray[(NSUInteger) section];
+    NSArray *tracks = _tracksArray[(NSUInteger) section];
+
+    view.separatorInsetLeft = tableView.separatorInset.left;
+    [view.titleLabel setText:currentAlbum.title];
+    [view.subtitleLabel setAttributedText:[[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@: %lld", NSLocalizedString(@"Tracks_count.albums", nil), (int64_t) tracks.count]
+                                                                                 attributes:@{NSForegroundColorAttributeName : [UIColor darkGrayColor]}]];
+}
 
 @end
